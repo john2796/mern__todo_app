ea2e44a73072320b91c89beba9a9661a02e28875
john2796 <jbmiranda22796@gmail.com>
2019-01-01 20:20:04 -0800

    adding label radio button

--- client/src/Components/CreateTodo.js
+++ client/src/Components/CreateTodo.js
@@ -25,46 +25,92 @@ export default class CreateTodo extends Component {
     });
   };
 
+  onChangeTodoPriority = () => {
+    console.log("testing");
+  };
+
   render() {
-    const {
-      todo_description,
-      todo_responsible,
-      todo_priority,
-      todo_completed
-    } = this.state;
+    const { todo_description, todo_responsible, todo_completed } = this.state;
 
     return (
-      <div>
-        <p>create todo</p>
+      <div style={{ marginTop: 20 }}>
+        <h3>Create new Todo</h3>
         <form onSubmit={this.handleSubmit}>
-          <input
-            type="text"
-            name="todo_description"
-            value={todo_description}
-            onChange={this.handleInputChange}
-            placeholder="description"
-          />
-          <input
-            type="text"
-            name="todo_responsible"
-            value={todo_responsible}
-            onChange={this.handleInputChange}
-            placeholder="responsible"
-          />
-          <input
-            type="text"
-            name="todo_priority"
-            value={todo_priority}
-            onChange={this.handleInputChange}
-            placeholder="priority"
-          />
-          <input
-            type="text"
-            name="todo_completed"
-            value={todo_completed}
-            onChange={this.handleInputChange}
-            placeholder="completed"
-          />
+          <div className="form-group">
+            <label>Description: </label>
+            <input
+              type="text"
+              name="todo_description"
+              value={todo_description}
+              onChange={this.handleInputChange}
+              className="form-control"
+            />
+          </div>
+          <div className="form-group">
+            <label>Responsible</label>
+            <input
+              type="text"
+              name="todo_responsible"
+              value={todo_responsible}
+              onChange={this.handleInputChange}
+              className="form-control"
+            />
+          </div>
+          {/* Low */}
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="priorityOptions"
+                value="Low"
+                className="form-check-input"
+                id="priorityLow"
+                checked={this.state.todo_priority === "Low"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">Low</label>
+            </div>
+          </div>
+          {/* Medium */}
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="priorityOptions"
+                value="Medium"
+                className="form-check-input"
+                id="priorityMedium"
+                checked={this.state.todo_priority === "Medium"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">Medium</label>
+            </div>
+          </div>
+          {/* High */}
+          <div className="form-group">
+            <div className="form-check form-check-inline">
+              <input
+                type="radio"
+                name="priorityOptions"
+                value="High"
+                className="form-check-input"
+                id="priorityHigh"
+                checked={this.state.todo_priority === "High"}
+                onChange={this.onChangeTodoPriority}
+              />
+              <label className="form-check-label">High</label>
+            </div>
+          </div>
+          <div className="form-group">
+            <label>Completed</label>
+            <input
+              type="text"
+              name="todo_completed"
+              value={todo_completed}
+              onChange={this.handleInputChange}
+              className="form-control"
+            />
+          </div>
 
           <button type="submit">submit</button>
         </form>
